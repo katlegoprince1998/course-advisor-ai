2126901d6ed1e2b83aad4a61a8e290acca955e92
katlegoprince1998 <princekatlego357@gmail.com>
2026-05-01 14:55:17 +0200

    first commit

---
+++ src/main/java/com/qda/ai_course_advisor/AiCourseAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.qda.ai_course_advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCourseAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCourseAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qda/ai_course_advisor/AiCourseAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qda.ai_course_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCourseAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
